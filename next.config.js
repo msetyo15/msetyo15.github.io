4205cfb3aae3c7d3af7db090495ef653fb0152cf
msetyo15 <manuelsetyo@gmail.com>
2022-08-20 07:04:07 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,12 +4,12 @@ const nextConfig = {
   swcMinify: true,
 
   // enabled during build, turn off during develop
-  // images: {
-  //   loader: "akamai",
-  //   path: "",
-  // },
-  // basePath: "/nextjs-pages", // enabled during build, turn off during develop
-  // assetPrefix: "/nextja-pages", //enabled during build, turn off during develop
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  basePath: "/nextjs-pages", // enabled during build, turn off during develop
+  assetPrefix: "/nextja-pages", //enabled during build, turn off during develop
 }
 
 module.exports = nextConfig
